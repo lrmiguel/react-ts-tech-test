a8dd04588c60831c2a639d95dbf13d77f6f148ae
lraise-stage <leonardoraise@stage.tech>
2025-05-07 12:59:13 -0300

    style: increase establishments table rows' font size

--- src/components/EstablishmentsTableRow.tsx
+++ src/components/EstablishmentsTableRow.tsx
@@ -1,8 +1,11 @@
+const tableRowsStyle = {
+  fontSize: "20px"
+}
 export const EstablishmentsTableRow: React.FC<{
   establishment: { [key: string]: string } | null | undefined;
 }> = ({ establishment }) => {
   return (
-    <tr>
+    <tr style={tableRowsStyle}>
       <td>{establishment?.BusinessName}</td>
       <td>{establishment?.RatingValue}</td>
     </tr>
