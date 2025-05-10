5f559307817b56bd2ad0bfc14779b2a2e73d6c0b
lraise-stage <leonardoraise@stage.tech>
2025-05-10 09:45:27 -0300

    test: enhance E2E tests with loading state checks, authority filtering, details page nav

--- cypress/e2e/paginated_establishments.cy.js
+++ cypress/e2e/paginated_establishments.cy.js
@@ -1,9 +1,45 @@
-
+Cypress.config('defaultCommandTimeout', 60000);
 describe('Paginated Establishments Table E2E', () => {
     it('displays loading on page change', () => {
       cy.visit('/');
+      cy.contains('Loading...').should('be.visible');
+      cy.contains('Loading...').should('not.exist'); 
       cy.contains('+').click();
       cy.contains('Loading...').should('be.visible');
     });
-    
+
+    it('returns to the previous page and displays data', () => {
+      cy.visit('/');
+      cy.contains('+').click();
+      cy.contains('Loading...').should('be.visible');
+      cy.contains('Loading...').should('not.exist');
+      cy.contains('2').should('exist');
+      cy.contains('-').click();
+      // check if cache works
+      cy.contains('Loading...').should('not.exist');
+      cy.contains('1').should('exist');
+    });
+
+    it('filters establishments by authority', () => {
+      cy.visit('/');
+      cy.get('div[id="authoritiesDropDown"]').click();
+      cy.get('div[id="authoritiesDropDown"]').within(() => {
+        cy.contains('Adur').click();
+      });
+      cy.contains('Loading...').should('be.visible');
+      cy.contains('Loading...').should('not.exist');
+      cy.get('table').within(() => {
+        cy.contains('ADSushi').should('exist');
+      });
+    });
+
+    it('navigates to the details page of an establishment', () => {
+      cy.visit('/');
+      cy.contains('!NOSH!').click();
+      cy.contains('Loading...').should('be.visible');
+      cy.contains('Loading...').should('not.exist');
+      cy.url().should('include', '/establishments');
+      cy.contains('Establishment Details').should('exist');
+      cy.contains('!NOSH!').should('exist');
+    });
 });
